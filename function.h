10fe22336cbfd16bda5f74380ec5ba48ee778942
hannypm <101863648+hannypm@users.noreply.github.com>
2022-03-19 14:40:49 +0700

    Add files via upload

---
+++ function.h
@@ -0,0 +1,24 @@
+int tambah(int x, int y)
+ {
+ 	int hasil;
+ 	hasil=x+y;
+ 	return hasil;
+ }
+ int kurang(int x, int y)
+ {
+ 	int hasil;
+ 	hasil=x-y;
+ 	return hasil;
+ }
+int kali(int x, int y)
+{
+	int hasil;
+	hasil=x*y;
+	return hasil;
+}
+int bagi(float x, float y)
+{
+	float hasil;
+	hasil=x/y;
+	return hasil;
+} 
